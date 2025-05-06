ed737ff89192456ccb898e8aa1d340c2aa272431
Juan Sobalvarro <sobalvarrog.juans@gmail.com>
2025-05-05 23:57:47 -0600

    refactor: renamed ttranking to backend

--- backend/players/views.py
+++ backend/players/views.py
@@ -4,14 +4,15 @@ from rest_framework.response import Response
 from rest_framework.decorators import action, api_view
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, AllowAny
 from rest_framework.pagination import PageNumberPagination
-from .models import Player, Ranking, COUNTRY_CHOICES
+from .models import Player, Ranking
+from .enums import CountryChoices
 from .serializers import PlayerSerializer, RankingSerializer
 
 PLAYERS_PER_PAGE = 6
 
 @api_view(['GET'])
 def country_choices(request):
-    countries = COUNTRY_CHOICES
+    countries = CountryChoices.choices
     return Response(countries)
 
 
--- backend/ttranking/urls.py
+++ backend/ttranking/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('api/', include('core.urls')),
     path('api/players/', include('players.urls')),
