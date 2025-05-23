c2c8bf3d1043c2f3191a4f2e6bfcf5f5618b987f
Juan Sobalvarro <sobalvarrog.juans@gmail.com>
2025-05-23 13:09:28 -0600

    fix: Fixing letsencrypt

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
 domains=(ttranking.juso-software.com)
-email="sobalvarrog_juans@gmail.com"  # CHANGE THIS
-staging=0  # Set to 1 for testing/staging rate limits
+email="sobalvarrog_juans@gmail.com"
+staging=0 # Set to 1 for testing
 
-# Create folders if needed
 mkdir -p ./certbot/conf ./certbot/www
 
-# Check if cert already exists
 if [ -d "./certbot/conf/live/${domains[0]}" ]; then
   echo "Certificate for ${domains[0]} already exists. Skipping..."
 else
   echo "### Requesting certificate for ${domains[*]} ..."
 
-  docker-compose run --rm certbot certonly \
+  docker-compose run --rm certbot sh -c "certbot certonly \
     --webroot -w /var/www/certbot \
     --email $email \
     --agree-tos \
     --no-eff-email \
-    -d ${domains[@]} \
-    $( [ $staging -ne 0 ] && echo "--staging" )
+    -d ${domains[*]} \
+    $( [ $staging -ne 0 ] && echo '--staging' )"
 
   echo "### Certificates created."
 fi
